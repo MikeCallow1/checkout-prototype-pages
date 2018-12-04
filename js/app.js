5d1f8a5e75045424c8753fa254e61a2d887eebb2
Mike Callow <mike@vashi.com>
2018-12-04 16:20:42 +0000

    protoype

---
+++ js/app.js
@@ -0,0 +1,166 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var VashiCheckout =
+/*#__PURE__*/
+function () {
+  function VashiCheckout() {
+    _classCallCheck(this, VashiCheckout);
+
+    this.init();
+  }
+
+  _createClass(VashiCheckout, [{
+    key: "init",
+    value: function init() {
+      console.log('Vashi Checkout Prototype');
+      this.loadingOverlay = document.querySelector('.loading-overlay');
+      this.addressToggles = document.querySelectorAll('.input-different-shipping input[type="radio"]');
+      this.bindLoginClick();
+      this.bindAddressToggles();
+      this.initTabs();
+    }
+  }, {
+    key: "bindLoginClick",
+    value: function bindLoginClick() {
+      var loginButton = document.querySelector('.login').querySelector('input[type="submit"]');
+      loginButton.addEventListener('click', this.handleLogin.bind(this));
+    }
+  }, {
+    key: "handleLogin",
+    value: function handleLogin(e) {
+      var _this = this;
+
+      e.preventDefault();
+      this.showLoadingSpinner();
+      this.timeout(1000).then(function () {
+        _this.hideLoadingSpinner();
+
+        _this.showBillingAddressSection();
+
+        _this.scrollToSection('.address--billing');
+      }).catch(function () {
+        _this.hideLoadingSpinner();
+
+        console.log('Login error');
+      });
+    }
+  }, {
+    key: "bindAddressToggles",
+    value: function bindAddressToggles() {
+      var _this2 = this;
+
+      this.addressToggles.forEach(function (radio) {
+        radio.addEventListener('change', function (e) {
+          if (e.target.id === 'billing:use_different') {
+            _this2.showLoadingSpinner();
+
+            _this2.timeout(1000).then(function () {
+              _this2.hideLoadingSpinner();
+
+              _this2.showShippingAddressSection();
+
+              _this2.scrollToSection('.address--shipping');
+            }).catch(function () {
+              _this2.hideLoadingSpinner();
+
+              console.log('Shipping address error');
+            });
+          } else {
+            _this2.hideShippingAddressSection();
+          }
+        });
+      });
+    }
+  }, {
+    key: "showLoadingSpinner",
+    value: function showLoadingSpinner() {
+      this.loadingOverlay.classList.add('loading-overlay--active');
+    }
+  }, {
+    key: "hideLoadingSpinner",
+    value: function hideLoadingSpinner() {
+      this.loadingOverlay.classList.remove('loading-overlay--active');
+    }
+  }, {
+    key: "showBillingAddressSection",
+    value: function showBillingAddressSection() {
+      document.querySelector('.address--billing').classList.remove('address--disabled');
+    }
+  }, {
+    key: "scrollToSection",
+    value: function scrollToSection(el) {
+      document.querySelector(el).scrollIntoView({
+        block: 'start',
+        behavior: 'smooth'
+      });
+    }
+  }, {
+    key: "showShippingAddressSection",
+    value: function showShippingAddressSection() {
+      document.querySelector('.address--shipping').classList.remove('address--hidden');
+      document.querySelector('.address--shipping').classList.remove('address--disabled');
+    }
+  }, {
+    key: "hideShippingAddressSection",
+    value: function hideShippingAddressSection() {
+      document.querySelector('.address--shipping').classList.add('address--hidden');
+    }
+  }, {
+    key: "initTabs",
+    value: function initTabs() {
+      var tabs = document.querySelectorAll('.tabs');
+      tabs.forEach(function (tab) {
+        var tabButton = tab.querySelectorAll('.tab');
+        tabButton.forEach(function (tab, i) {
+          tab.addEventListener('click', function (e) {
+            e.preventDefault();
+            setInactivePanels();
+            setActivePanel(i, tab);
+          });
+        });
+
+        function setInactivePanels() {
+          var tabButton = tab.querySelectorAll('.tab');
+          tabButton.forEach(function (button) {
+            button.classList.remove('tab--active');
+          });
+          var tabContent = tab.querySelectorAll('.tab__content');
+          tabContent.forEach(function (tab) {
+            tab.classList.remove('tab__content--active');
+          });
+        }
+
+        function setActivePanel(i, tabButton) {
+          var tabContent = tab.querySelectorAll('.tab__content');
+          tabContent[i].classList.add('tab__content--active');
+          tabButton.classList.add('tab--active');
+        }
+      });
+    }
+    /**
+     * Timeout for spoofing ajax reqs
+     * 
+     * @param {Number} ms 
+     */
+
+  }, {
+    key: "timeout",
+    value: function timeout(ms) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          resolve();
+        }, ms);
+      });
+    }
+  }]);
+
+  return VashiCheckout;
+}();
+
+new VashiCheckout();
